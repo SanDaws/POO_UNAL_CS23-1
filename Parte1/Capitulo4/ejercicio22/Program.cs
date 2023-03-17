760ce02855af37c8461ba78899d93290bf8af685
Daws <75854211+SanDaws@users.noreply.github.com>
2023-03-17 10:08:52 -0500

    ejercicios Propuestos

---
+++ Parte1/Capitulo4/ejercicio22/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace ejercicio22;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Parte1/Capitulo4/ejercicio24/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace ejercicio24;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
