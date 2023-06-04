4f93aa0ca15cdcd2ea4570ae81427ba1ea32fc34
g-agudelo <126741130+g-agudelo@users.noreply.github.com>
2023-06-03 20:41:44 -0500

    Add files via upload

---
+++ Trabajo4/Tarea4_2/Apartamento.java
@@ -0,0 +1,14 @@
+package Tarea4_2;
+
+public class Apartamento extends Vivienda {
+    
+    public Apartamento(int ID,float area,String direccion,String tipoinmueble,int nhabitaciones,int nbaños){
+        super(ID, area, direccion, tipoinmueble, nhabitaciones, nbaños);
+    }
+
+    public void imprimir(){
+        super.imprimir();
+    }
+    
+}   
+
---
+++ Trabajo4/Tarea4_2/CasaUrbana.java
@@ -0,0 +1,13 @@
+package Tarea4_2;
+
+public class CasaUrbana extends Vivienda{
+
+    public CasaUrbana(int ID,float area,String direccion,String tipoinmueble,int nhabitaciones,int nbaños){
+        super(ID, area, direccion, tipoinmueble, nhabitaciones, nbaños);
+    }
+
+    public void imprimir(){
+        super.imprimir();
+    }
+    
+}
